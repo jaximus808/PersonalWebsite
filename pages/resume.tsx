df73276e0d10cfc556b9f251f35ffc3bfeef04d2
observer <j.k.poentis@wustl.edu>
2025-07-17 19:37:51 -0500

    feat: updated resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -5,7 +5,7 @@ export default function PDFPage() {
     
     <div className="flex flex-col h-screen">
         <Header/>
-        <iframe className="mt-4 w-full flex-grow" src='./SWE_Resume_Jaxon_4_9_2025.pdf' />
+        <iframe className="mt-4 w-full flex-grow" src='./resume_norm_swe_Jaxon_Poentis-07-01-2025.pdf' />
     </div>
     
   )
