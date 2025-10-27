627766bee24e3dc68e6ff0251dbed9102d66bb0c
observer <j.k.poentis@wustl.edu>
2025-10-27 11:56:19 -0500

    package upgrades

--- next.config.js
+++ next.config.js
@@ -2,23 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  turbopack: {}, // Add this line
   images: {
     // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'drive.google.com',
-    //     port: '',
-    //   },
+    // {
+    // protocol: 'https',
+    // hostname: 'drive.google.com',
+    // port: '',
+    // },
     // ],
     domains:['drive.google.com','cdn.discordapp.com']
   },
   compress:true,
-	webpack(config, { dev }) {
-		if(dev) {
-			config.devtool = 'react_devtools_backend_compact'
-		}
-		return config;
-	}
+  webpack(config, { dev }) {
+    if(dev) {
+      config.devtool = 'react_devtools_backend_compact'
+    }
+    return config;
+  }
 }
-
 module.exports = nextConfig
